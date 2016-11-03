71fae61a2280b443147c6418adf9937445d8dc51
jialuc <jialuc@andrew.cmu.edu>
2016-11-03 15:43:28 -0400

    Turning in Homework #1.

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -1,11 +1,16 @@
+//get all the operators objects
 var optBtn = document.querySelectorAll('.operator input');
+//operators array
 var operators = ['+', '-', '*', '/'];
 for (var i = 0; i < optBtn.length; i++) {
+    //operator buttons event handler
     optBtn[i].onclick = function (e) {
+        //get input values and get get answer element
         var inputX = parseInt(document.getElementById("x").value);
         var inputY = parseInt(document.getElementById("y").value);
         var ansText = document.getElementById("Answer");
 
+        //determine which operation to do
         if (this.value == operators[0]) {
             ansText.value = inputX + inputY;
         } else if(this.value == operators[1]) {
